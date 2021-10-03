8aaab7de7acf42b849beee7ec5c4ed97cdd5f7b6
meenekkodath <smithamanu76@gmail.com>
2021-10-03 09:16:40 -0500

    initial commit

--- src/main/java/com/spring/learning/dependency/injection/DependancyStarter.java
+++ src/main/java/com/spring/learning/dependency/injection/DependancyStarter.java
@@ -1,12 +1,21 @@
 package com.spring.learning.dependency.injection;
 
+import com.spring.learning.dependency.injection.search.BinarySearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependancyStarter {
+    //what are the beans
+    //whar are the dependcies for the bean
+    //where to search the beans
+
     public static void main(String[] args) {
-        SpringApplication.run(DependancyStarter.class);
+       ApplicationContext context = SpringApplication.run(DependancyStarter.class);
+        BinarySearch search = context.getBean(BinarySearch.class);
+       int result = search.search(new int[]{20,5,2,200},2);
+        System.out.println(result);
 
     }
 }
--- src/main/java/com/spring/learning/dependency/injection/search/BinarySearch.java
+++ src/main/java/com/spring/learning/dependency/injection/search/BinarySearch.java
@@ -2,8 +2,11 @@ package com.spring.learning.dependency.injection.search;
 
 import com.spring.learning.dependency.injection.algorithm.SortingAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BinarySearch {
+    //dependency```````
     @Autowired
     SortingAlgorithm bubbleSort;
     public int search(int[] numbers,int num){
